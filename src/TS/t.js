50f9ae6b4f9cbb18b79dc57e5b0677752df073af
worawut-kr <worawut.kr@outlook.co.th>
2024-01-28 22:14:10 +0700

    ts

---
+++ src/TS/t.js
@@ -0,0 +1,577 @@
+var numbers = [1, 3, 5, 7, 9, 8, 55, 2];
+function selectionSort(arr) {
+    var i = 0;
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        /////////////////////////////////////
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        ////////////////////////////////////
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        /////////////////////////////////////
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        ////////////////////////////////////
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        /////////////////////////////////////
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        ////////////////////////////////////
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        /////////////////////////////////////
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        ////////////////////////////////////
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        /////////////////////////////////////
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        ////////////////////////////////////
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        /////////////////////////////////////
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        ////////////////////////////////////
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    if (i < arr.length) {
+        var min = i;
+        var holding = arr[i];
+        var j = i + 1;
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+            j++;
+        }
+        arr[i] = arr[min];
+        arr[min] = holding;
+        i++;
+    }
+    return arr;
+}
+var mm = selectionSort(numbers);
+console.log(mm);
---
+++ src/TS/t.ts
@@ -0,0 +1,581 @@
+const numbers = [1, 3, 5, 7, 9, 8, 55, 2]
+
+function selectionSort(arr: number[]): number[] {
+    let i = 0
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+/////////////////////////////////////
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+////////////////////////////////////
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+/////////////////////////////////////
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+////////////////////////////////////
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+/////////////////////////////////////
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+////////////////////////////////////
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+/////////////////////////////////////
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+////////////////////////////////////
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+/////////////////////////////////////
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+////////////////////////////////////
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+/////////////////////////////////////
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+////////////////////////////////////
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    if (i < arr.length) {
+        let min = i
+        let holding = arr[i]
+        let j = i + 1
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        if (j < arr.length) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+            j++
+        }
+        arr[i] = arr[min]
+        arr[min] = holding
+        i++
+    }
+    
+
+    return arr
+}
+
+let mm = selectionSort(numbers)
+console.log(mm)
